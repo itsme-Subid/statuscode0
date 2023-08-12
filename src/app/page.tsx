025600289707558cc31a9fe5c9335716afe398d0
Niladri <rickyrickado14@gmail.com>
2023-08-12 19:14:24 +0530

    video call added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
+"use client";
+
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 const Home = () => {
-  return <div>Home</div>;
+  const { loginWithRedirect,logout, isAuthenticated } = useAuth0();
+  const { user } = useAuth0();
+  return (<div>Home{!isAuthenticated && <button onClick={() => loginWithRedirect()}>Login</button>}
+      {isAuthenticated && user && <button
+        onClick={() =>
+          logout({ logoutParams: { returnTo: window.location.origin } })
+        }
+      > {user.name}
+        Log Out
+      </button>}</div>);
 };
 
 export default Home;
